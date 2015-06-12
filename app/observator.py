57dd5ecabf55aa02b660ff0ef9979da72cfd4dea
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2015-06-12 16:52:27 +0300

    Data structure fixes, now import works

--- app/observator.py
+++ app/observator.py
@@ -1,4 +1,3 @@
-
 import datetime
 
 from .models import Observer, Observations
@@ -7,36 +6,38 @@ from collections import namedtuple
 
 Measurement = namedtuple('Measurement', ['moment', 'measurement'])
 
+
 def read(docs):
-	data = {}
-	for doc in docs:
-		data[doc] = {"observations" : []}
-		for l in open(doc):
-			first, second = l.strip().split(" ")
-			if first.isalpha():
-				data[doc][first] = second
-			else:
-				try:
-					dt = datetime.datetime.strptime(first, '%d.%m.%Y')
-					meas = Measurement(dt, second)
-					data[doc]["observations"].append(meas)
-				except ValueError, e:
-					print "Datetime conversion error", doc, first
-	return data
+    data = {}
+    for doc in docs:
+        data[doc] = {"observations": []}
+        for l in open(doc):
+            first, second = l.strip().split(" ")
+            if first.isalpha():
+                data[doc][first] = second
+            else:
+                try:
+                    dt = datetime.datetime.strptime(first, '%d.%m.%Y')
+                    meas = Measurement(dt, second)
+                    data[doc]["observations"].append(meas)
+                except ValueError, e:
+                    print "Datetime conversion error", doc, first
+    return data
+
 
 def save(data):
-	for item_id, obj in data:
-		d, newd = Observer.objects.get_or_create(
-			name=item_id,
-			location=item['PISTE'],
-			min=item['MIN'],
-			max=item['MAX'],
-			keski=item['KESKI'],
-			halymin=item['HALYMIN'],
-			halymax=item['HALYMAX'])
-		for obs in obj['observations']:
-			dobs = Observations(
-				moment=obs.moment,
-				measurement=obs.measurement,
-				observer=d)
-			dobs.save()
+    for item_id, item in data.items():
+        d, newd = Observer.objects.get_or_create(
+            name=item_id,
+            location=item['PISTE'],
+            min=item['MIN'],
+            max=item['MAX'],
+            avg=item['KESKI'],
+            halymin=item.get('HALYMIN', None),
+            halymax=item.get('HALYMAX', None))
+        for obs in item['observations']:
+            dobs = Observations(
+                moment=obs.moment,
+                measurement=obs.measurement,
+                observer=d)
+            dobs.save()
