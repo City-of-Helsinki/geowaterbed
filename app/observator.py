1b1d6bcd00f35ef11049a923879e4664c098fb8f
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2015-09-16 19:59:01 +0300

    Saving Postgres GIS projection settings

--- app/observator.py
+++ app/observator.py
@@ -77,3 +77,8 @@ def update_coord(coords):
         d.loc_x = item[0]
         d.loc_y = item[1]
         d.save()
+
+POSTGRES_PROJECTION_SETTINGS = """
+INSERT INTO "spatial_ref_sys" ("srid","auth_name","auth_srid","srtext","proj4text") VALUES (3879,'EPSG',3879,'PROJCS["ETRS89 / ETRS-GK25FIN",GEOGCS["ETRS89",DATUM["European_Terrestrial_Reference_System_1989",SPHEROID["GRS 1980",6378137,298.257222101,AUTHORITY["EPSG","7019"]],TOWGS84[0,0,0,0,0,0,0],AUTHORITY["EPSG","6258"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4258"]],UNIT["metre",1,AUTHORITY["EPSG","9001"]],PROJECTION["Transverse_Mercator"],PARAMETER["latitude_of_origin",0],PARAMETER["central_meridian",25],PARAMETER["scale_factor",1],PARAMETER["false_easting",25500000],PARAMETER["false_northing",0],AUTHORITY["EPSG","3879"],AXIS["Northing",NORTH],AXIS["Easting",EAST]]','+proj=tmerc +lat_0=0 +lon_0=25 +k=1 +x_0=25500000 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs ');
+select ST_AsText(ST_Transform(ST_GeomFromText('POINT(25496807.262 6673221.252)', 3879), 4326));
+"""
